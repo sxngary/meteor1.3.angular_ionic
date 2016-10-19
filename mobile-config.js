91f47d330113493a1a3a64795fb14bb32d821f95
Dinesh Kumar <dinesh.kumar@smartdatainc.net>
2016-10-19 19:46:59 +0530

    latest changes for android app

--- mobile-config.js
+++ mobile-config.js
@@ -12,11 +12,14 @@ App.info({
 App.icons({
 	'android_mdpi': 'icons/icon-48.png',
 	'android_hdpi': 'icons/icon-72.png',
-  	'android_xhdpi': 'icons/icon-96.png',
-  	'android_xxhdpi': 'icons/icon-144.png',
-  	'android_xxxhdpi': 'icons/icon-192.png'
+  'android_xhdpi': 'icons/icon-96.png',
+  'android_xxhdpi': 'icons/icon-144.png',
+  'android_xxxhdpi': 'icons/icon-192.png'
 });
 
 App.launchScreens({
-	'android_xhdpi_portrait': 'splash/screen-xhdpi-720.png'
+  'android_mdpi_portrait': 'splash/splash-mdpi.png',
+  'android_hdpi_portrait': 'splash/splash-hdpi.png',
+	'android_xhdpi_portrait': 'splash/splash-xhdpi.png',
+  'android_xxhdpi_portrait': 'splash/splash-xxhdpi.png'
 });
