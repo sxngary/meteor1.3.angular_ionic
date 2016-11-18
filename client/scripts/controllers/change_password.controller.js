c304c0e04e3059eb44bffe4d28decccbf0b4beb1
Dinesh Kumar1 <dinesh.kumar1@isTeams-Mac-Mini3-2.local>
2016-11-18 09:45:49 +0530

    user profile section integration

--- client/scripts/controllers/change_password.controller.js
+++ client/scripts/controllers/change_password.controller.js
@@ -4,7 +4,74 @@ export default class ChangePasswordCtrl extends Controller {
   constructor() {
     super(...arguments);
 
+    this.formData = {
+      curr_passwd: null,
+      new_passwd: null,
+      repeat_passwd: null
+    }
+
+    //Add custom validation function for password.
+    this.$validation
+        .setExpression({
+            passwdValidator: function (value, scope, element, attrs, param) {
+              if(value){
+                var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+                var nv =  re.test(value);
+                if(nv){
+                  return true;
+                }
+              }
+            }
+        })
+        .setDefaultMsg({
+            passwdValidator: {
+                error: "At least 1 digit, 1 lowercase, 1 uppercase and min length 6.",
+                success: ''
+            }
+        });
+
+
+    //Add custom validation function for confirm password.
+    this.$validation
+        .setExpression({
+            confirmPasswd: function (value, scope, element, attrs, param) {
+              if(value) return (value === scope.password.formData.new_passwd);
+            }
+        })
+        .setDefaultMsg({
+            confirmPasswd: {
+                error: "Passwords don't match.",
+                success: ''
+            }
+        });
+  }
+
+  update(form) {
+    let _this = this;
+    _this.$validation.validate(form)
+    .success(function(){
+        let formData = _this.formData;
+        //console.log("formData:", formData);
+
+        Accounts.changePassword(formData.curr_passwd, formData.new_passwd, function(err) {
+          if (err) {
+            //console.log("err:", err);
+            return;
+          }
+          _this.$ionicLoading.show({ template: 'Password updated successfully.', noBackdrop: true });
+          Meteor.setTimeout(function(){
+            _this.$ionicLoading.hide();
+          }, 1000);
+        })
+    })
+    .error(function(err){
+        //console.log("validation " + err);
+        _this.$ionicLoading.show({ template: 'Invalid form data.', noBackdrop: true });
+        Meteor.setTimeout(function(){
+          _this.$ionicLoading.hide();
+        }, 1000);
+    });
   }
 }
 
-ChangePasswordCtrl.$inject = ['$state'];
+ChangePasswordCtrl.$inject = ['$state', '$ionicLoading', '$validation'];
--- client/scripts/controllers/settings.controller.js
+++ client/scripts/controllers/settings.controller.js
@@ -4,14 +4,83 @@ export default class SettingsCtrl extends Controller {
   	constructor() {
 	    super(...arguments);
 
+		const profile = this.currentUser && this.currentUser.profile;
+	    this.profile = profile;
+
+		this.formData = {
+			firstname: profile.firstname,
+			lastname: profile.lastname,
+			zip_code: profile.zip_code
+		}
+
+		//Add custom validation function for zip code.
+        this.$validation
+            .setExpression({
+                zipValidator: function (value, scope, element, attrs, param) {
+                    if(value) return (value.toString().length <= Number(param) &&  value.toString().length >= 3);
+                }
+            })
+            .setDefaultMsg({
+                zipValidator: {
+                    error: 'Enter atleast 3 digits and not greater than 6 digits!',
+                    success: ''
+                }
+            });
+
+		//Add custom validation function for first & last name.
+        this.$validation
+            .setExpression({
+                nameValidator: function (value, scope, element, attrs, param) {
+                    if(value){
+                        var re = /^[a-z ,.'-]+$/i;
+                        var nv =  re.test(value);
+                        if(nv){
+                            return true;
+                        }
+                    }
+                }
+            })
+            .setDefaultMsg({
+                nameValidator: {
+                    error: "Name should be valid!",
+                    success: ''
+                }
+            });
   	}
+
+	update(form) {
+		let currentUser = this.currentUser;
+		let formData = this.formData;
+		let $ionicLoading = this.$ionicLoading;
+
+		_this = this;
+		_this.$validation.validate(form)
+		.success(function(){
+			Meteor.users.update({_id: currentUser._id}, {$set: {
+				"profile.firstname": formData.firstname,
+				"profile.lastname": formData.lastname,
+				"profile.zip_code": formData.zip_code
+			}});
+			$ionicLoading.show({ template: 'Profile updated successfully.', noBackdrop: true });
+			Meteor.setTimeout(function(){
+				$ionicLoading.hide();
+			}, 1000);
+		})
+		.error(function(err){
+			//console.log("validation " + err);
+			_this.$ionicLoading.show({ template: 'Invalid form data.', noBackdrop: true });
+			Meteor.setTimeout(function(){
+			_this.$ionicLoading.hide();
+			}, 1000);
+		});
+	}
 
 	logout() {
 		this.$ionicLoading.show({ template: 'Logging out. Please wait ...', noBackdrop: true });
         Meteor.logout((err) => {
 	      	if (err){ 
 	      		this.$ionicLoading.hide(); 
-	      		console.log(err);
+	      		//console.log(err);
 	      	}else{
 	      		this.$ionicLoading.hide(); 
 	      		this.$state.go('login');
@@ -20,4 +89,4 @@ export default class SettingsCtrl extends Controller {
 	}
 }
 
-SettingsCtrl.$inject = ['$state', '$ionicLoading'];
+SettingsCtrl.$inject = ['$state', '$ionicLoading', '$validation'];
